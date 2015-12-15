dca8bae29c08bf9bc87df3039868674a142e57d0
Alex Honor <alexhonor@yahoo.com>
2015-12-15 14:02:34 -0800

    first commit

---
+++ tests/update-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m rundeck-acls -p update [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "update"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
